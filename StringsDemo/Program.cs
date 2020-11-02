4127f20ae85ae5b54c19896b04992de8bbb54201
Donnel <donnel206@hotmail.com>
2020-11-02 11:31:57 +0100

    Add project files.

---
+++ StringsDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string drinks = "Coffee @@@@@   !!!! *** water cocacola pepsi 7up wine";
+
+
+            char[] splitCharacters = { ' ', '!', '@', '[', ']', '!', '$', '%', 'µ', '^', };
+
+            string[] splitDrinks = drinks.Split(splitCharacters,StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var drink in splitDrinks)
+            {
+                Console.WriteLine(drink);
+            }
+        }
+    }
+}
